bef4635766aef0ccb6744eb95117f064ebbbe045
Aaethrix <tahsinahmedpersonal@gmail.com>
2026-06-13 14:14:44 +0600

    Sum

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int a, b, sum;
+
+    cout<< "Enter the Values: ";
+    cin>> a >> b;
+    sum = a + b;
+    cout<< "Result: "<< sum;
+    
+}
